85d246d208a65826f42d928b10cdd8c2e6a47cbf
Jeffrey Johnson <jjohnson@jajsoftware.com>
2021-12-10 17:03:11 -0500

    Configured MVC and HTTPS redirecting.

--- MapAYak/Program.cs
+++ MapAYak/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();

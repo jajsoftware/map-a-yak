053dbf5f1632c94c17a3d4a3cff5474e0e0c75a4
Jeffrey Johnson <jjohnson@jajsoftware.com>
2021-12-11 03:22:14 -0500

    Added Leaflet using OpenStreetMap as tile server. Set up site to just show the map as the entirety of the page. Set up Git to ignore the lib folder but will restore packages upon building in VS.

---
+++ MapAYak/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿var map = L.map('map');
+
+var mapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var mapOptions = {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+};
+
+L.tileLayer(mapUrl, mapOptions).addTo(map);
+
+map.setView([43.5, -84.5], 7); // Just centers Michigan for now.
